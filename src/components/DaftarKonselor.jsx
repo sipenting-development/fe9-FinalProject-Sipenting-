53567d094e773d3d23ddb1cb87f836af861022b5
kharisma tara mukti <kharismataram@gmail.com>
2023-05-30 07:03:00 +0700

    done componet daftar konselor

--- src/components/DaftarKonselor.jsx
+++ src/components/DaftarKonselor.jsx
@@ -2,21 +2,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { Button, Col, Row } from "react-bootstrap";
 
-const DaftarKonselor = ({ img, nama, pengalaman }) => {
+const DaftarKonselor = ({ img, nama, pengalaman, ratings }) => {
   return (
     <Row className="conselor-box">
       <Col xs={4}>
-        <img className="conselor-photo" src={img} alt="konseler1" />
+        <img className="conselor-photo" src={img} alt="konselor1" style={{ width: "100%", maxWidth: "200px" }} />
       </Col>
       <Col xs={8}>
         <h4 className="font-weight-bold">{nama}</h4>
         <p>{pengalaman}</p>
         <div className="rating-group">
-          <FontAwesomeIcon icon={faStar} className="mr-1" style={{ fontSize: "25px" }} />
-          <FontAwesomeIcon icon={faStar} className="mr-1" style={{ fontSize: "25px" }} />
-          <FontAwesomeIcon icon={faStar} className="mr-1" style={{ fontSize: "25px" }} />
-          <FontAwesomeIcon icon={faStar} className="mr-1" style={{ fontSize: "25px" }} />
-          <FontAwesomeIcon icon={faStar} className="mr-1" style={{ fontSize: "25px" }} />
+          {ratings.map((rating, index) => (
+            <FontAwesomeIcon key={index} icon={faStar} className="mr-1" style={{ fontSize: "25px", color: rating === "yellow" ? "yellow" : "gray" }} />
+          ))}
         </div>
         <Button className="btn btn-primary mt-3 rounded-pill px-4 py-2">Konsultasi</Button>
       </Col>
