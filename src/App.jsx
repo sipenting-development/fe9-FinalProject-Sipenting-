32ff1b6eaa954b18eceed0f12b1d3ffd9e5637e4
kharisma tara mukti <kharismataram@gmail.com>
2023-06-07 21:27:33 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 import CekImt from "./pages/CekImt";
-import Konsultasi from "./pages/Konsultasi";
-=======
 import LandingPage from "./pages/LandingPage";
->>>>>>> dev01
+
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Konsultasi />
-      <CekImt />
-=======
-      <LandingPage />
->>>>>>> dev01
+   <LandingPage/>
+
     </>
   );
 }
