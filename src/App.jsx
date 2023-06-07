2ace02b13a3ad0853ff79ae2a56814ad8ed7c686
kharisma tara mukti <kharismataram@gmail.com>
2023-06-07 23:46:34 +0700

    up me artikel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import CekImt from "./pages/CekImt";
 import LandingPage from "./pages/LandingPage";
 import Konsultasi from "./pages/Konsultasi";
 import { Route, Routes } from "react-router-dom";
+import Artikel from "./pages/Artikel";
 function App() {
   return (
     <>
@@ -9,7 +10,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/konsultasi" element={<Konsultasi />} />
         <Route path="/cekgizi" element={<CekImt />} />
-        {/* <Route path="/artikel" element={<Artikel />} /> */}
+        <Route path="/artikel" element={<Artikel />} />
         {/* <Route path="/chat" element={<ChatBox />} /> */}
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
