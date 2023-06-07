6388db8a5aa6e79dc5c7019c3bc751bd84fb291a
kharisma tara mukti <kharismataram@gmail.com>
2023-06-07 23:20:40 +0700

    me landing dan cekgizi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import CekImt from "./pages/CekImt";
 import LandingPage from "./pages/LandingPage";
-
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-   <LandingPage/>
-
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/konsultasi" element={<Konsultasi />} /> */}
+        <Route path="/cekgizi" element={<CekImt />} />
+        {/* <Route path="/artikel" element={<Artikel />} /> */}
+        {/* <Route path="/chat" element={<ChatBox />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,46 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Button from "react-bootstrap/Button";
-import "./navbarsipenting.css";
+import React from "react";
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
+import { NavLink, useLocation } from "react-router-dom";
+import "./NavbarSipenting.css";
 
 function NavbarSipenting() {
+  const location = useLocation();
+
+  const getNavLinkStyle = (path) => {
+    if (path === location.pathname) {
+      return { color: "#54BCA4", fontWeight: "bold" };
+    }
+    return {};
+  };
+
   return (
     <Navbar bg="white" expand="lg" fixed="top" className="nav shadow-sm">
       <Container>
         <Navbar.Brand href="#logo">
-          <img
-            src="https://res.cloudinary.com/damsz0xlx/image/upload/v1683201272/Assets/navbar_bfjxem.png"
-            alt=""
-            width="150"
-          />
+          <img src="https://res.cloudinary.com/damsz0xlx/image/upload/v1683201272/Assets/navbar_bfjxem.png" alt="" width="150" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="reponsive-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="nav-item navbar-nav ms-auto justify-content-end flex-grow-1 pe-3">
-            <Nav.Link
-              to="/"
-              className="nav-link active pe-5"
-              aria-current="page"
-              style={{ color: "#54BCA4", fontWeight: "bold" }}
-            >
+          <Nav className="navbar-nav ms-auto justify-content-end flex-grow-1 pe-3">
+            <Nav.Link as={NavLink} exact={true.toString()} to="/" className="nav-link active pe-5" style={getNavLinkStyle("/")}>
               Beranda
             </Nav.Link>
-            <Nav.Link to="/" className="nav-link pe-5">
-              Layanan Kami
+            <Nav.Link as={NavLink} exact={true.toString()} to="/konsultasi" className="nav-link pe-5" style={getNavLinkStyle("/konsultasi")}>
+              Konsultasi
             </Nav.Link>
-            <Nav.Link to="/" className="nav-link pe-5">
-              Cek IMT
+            <Nav.Link as={NavLink} exact={true.toString()} to="/cekgizi" className="nav-link pe-5" style={getNavLinkStyle("/cekgizi")}>
+              Cek Gizi
             </Nav.Link>
-            <Nav.Link to="/" className="nav-link pe-5">
+            <Nav.Link as={NavLink} exact={true.toString()} to="/artikel" className="nav-link pe-5" style={getNavLinkStyle("/artikel")}>
               Artikel
             </Nav.Link>
           </Nav>
-          <Button variant="outline-success me-4" className="btndaftar">Daftar</Button>
-          <Button variant="outline-success" className="btnmasuk">Masuk</Button>
+          <Button variant="outline-success me-4" className="btndaftar">
+            Daftar
+          </Button>
+          <Button variant="outline-success" className="btnmasuk">
+            Masuk
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
