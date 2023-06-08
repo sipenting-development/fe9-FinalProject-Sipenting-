1f8b675112bf09138ce7d0133ab9723465d28a6a
kharisma tara mukti <kharismataram@gmail.com>
2023-06-08 22:19:39 +0700

    chat selesai

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Crisp } from "crisp-sdk-web";
+import { Stack } from "react-bootstrap";
+import "./notfound.css";
 
 class ChatBox extends Component {
   componentDidMount() {
@@ -7,7 +9,16 @@ class ChatBox extends Component {
   }
 
   render() {
-    return <div>My Awesome App</div>;
+    return (
+      <div>
+        <Stack gap={3}>
+          <div className="text-center mt-5">
+            <img src="https://res.cloudinary.com/damsz0xlx/image/upload/v1686235709/bro_eto8s3.png" alt="error" style={{ width: 350, height: 350 }} className="mb-3 mt-4" />
+            <p className="fw-bold fs-4 mt-3">Silakan Melakukan Konsultasi</p>
+          </div>
+        </Stack>
+      </div>
+    );
   }
 }
 
