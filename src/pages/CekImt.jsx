1dd4b2779e8b5a896971b1b21998d1fbb1eb1c36
kharisma tara mukti <kharismataram@gmail.com>
2023-06-08 11:10:01 +0700

    link konselor

--- src/pages/CekImt.jsx
+++ src/pages/CekImt.jsx
@@ -24,15 +24,15 @@ function CekImt(params) {
             <BmiCalculator />
           </BmiProvider>
           <Container className="text-center text-lg-start mt-3 mt-md-5 mb-5">
-            <h3 className="fw-bold mt-7 mb-4 pt-5 fs-3 layanan1">Layanan Konsultasi</h3>
+            <h3 className="fw-bold mt-7 mb-4 pt-5 fs- layanan1">Layanan Konsultasi</h3>
             <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center">
               <div className="mb-3 mb-lg-0">
-                <p className="ms-0 fs-5 layanan 2">Dapatkan konsultasi medis anak yang cepat dan mudah dari konselor kami yang berpengalaman. Konsultasi medis anak yang nyaman dan aman dari rumah Anda sendiri</p>
+                <p className="ms-0 fs-6 layanan 2">Dapatkan konsultasi medis anak yang cepat dan mudah dari konselor kami yang berpengalaman. Konsultasi medis anak yang nyaman dan aman dari rumah Anda sendiri</p>
               </div>
               <div className="p-4 text-end">
-                <a href="#" className="btn rounded-5 px-5 py-2 text-sm-center fs-6 text-white fw-semibold d-inline-block" style={{ backgroundColor: "#54bca4" }}>
+                <Link to="/konsultasi" className="btn rounded-5 px-5 py-2 text-sm-center fs-6 text-white fw-semibold d-inline-block" style={{ backgroundColor: "#54bca4" }}>
                   Konseling
-                </a>
+                </Link>
               </div>
             </div>
           </Container>
