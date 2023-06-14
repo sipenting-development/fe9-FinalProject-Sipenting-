4fa8319583655caebbf5ca7e6496f9dfcdd350b8
WhataD?0wD <ditakusuma66@gmail.com>
2023-06-14 20:17:16 +0700

    add list article landingpage

--- src/pages/CekImt.jsx
+++ src/pages/CekImt.jsx
@@ -17,7 +17,9 @@ function CekImt(params) {
           <HeroCekKonsul
             img={"https://res.cloudinary.com/damsz0xlx/image/upload/v1683201271/Assets/cekstunting_sxkcbq.png"}
             title={"Cek Status Gizi !"}
-            descrip={"Pantau pertumbuhan dan perkembangan anak Anda secara berkala. Untuk mengetahui seberapa sehat anak Anda. Cek kondisi anak Anda bisa menggunakan layanan kami. "}
+            descrip={
+              "Pantau pertumbuhan dan perkembangan anak Anda secara berkala. Untuk mengetahui seberapa sehat anak Anda. Cek kondisi anak Anda bisa menggunakan layanan kami. "
+            }
           />
 
           <BmiProvider>
@@ -25,26 +27,31 @@ function CekImt(params) {
           </BmiProvider>
 
           <section className="artikel">
-          <Container className="text-start">
-            <h3 className="fw-bold ms-3 mb-4 pt-2 fs-3">
-              Artikel Terkait
-            </h3>
-            <Container>
-              <Row>
-                <ArticleList />
-              </Row>
+            <Container className="text-start">
+              <h3 className="fw-bold ms-3 mb-4 pt-2 fs-3">Artikel Terkait</h3>
+              <Container>
+                <Row>
+                  <ArticleList />
+                </Row>
+              </Container>
             </Container>
-          </Container>
-        </section>
+          </section>
 
           <Container className="text-center text-lg-start mt-3 mt-md-5 mb-5">
             <h3 className="fw-bold mt-7 mb-4 pt-5 fs- layanan1">Layanan Konsultasi</h3>
             <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center">
               <div className="mb-3 mb-lg-0">
-                <p className="ms-0 fs-6 layanan 2">Dapatkan konsultasi medis anak yang cepat dan mudah dari konselor kami yang berpengalaman. Konsultasi medis anak yang nyaman dan aman dari rumah Anda sendiri</p>
+                <p className="ms-0 fs-6 layanan 2">
+                  Dapatkan konsultasi medis anak yang cepat dan mudah dari konselor kami yang berpengalaman. Konsultasi
+                  medis anak yang nyaman dan aman dari rumah Anda sendiri
+                </p>
               </div>
               <div className="p-4 text-end ">
-                <Link to="/konsultasi" className=" konseling btn rounded-5 px-5 py-2 text-sm-center fs-6 text-white fw-semibold d-inline-block " style={{ backgroundColor: "#54bca4", fontFamily: "Poppins" }}>
+                <Link
+                  to="/konsultasi"
+                  className=" konseling btn rounded-5 px-5 py-2 text-sm-center fs-6 text-white fw-semibold d-inline-block "
+                  style={{ backgroundColor: "#54bca4", fontFamily: "Poppins" }}
+                >
                   Konseling
                 </Link>
               </div>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,16 +3,28 @@ import Footer from "../components/Footer";
 import HeroLanding from "../components/HeroLanding";
 import DataStunting from "../components/DataStunting";
 import CarouselLayanan from "../components/CarouselLayanan";
+import { Container, Col, Row } from "react-bootstrap";
+import ArticleList from "../components/ListArtikel";
 
 function LandingPage() {
-  return ( 
-  <>
-  <Navbar />
-  <HeroLanding />
-  <DataStunting />
-  <CarouselLayanan />
-  <Footer />
-  </>
+  return (
+    <>
+      <Navbar />
+      <HeroLanding />
+      <DataStunting />
+      <CarouselLayanan />
+      <section className="artikel">
+        <Container className="text-start">
+          <h3 className="fw-bold ms-3 mb-4 pt-2 fs-3">Artikel Terkait</h3>
+          <Container>
+            <Row>
+              <ArticleList />
+            </Row>
+          </Container>
+        </Container>
+      </section>
+      <Footer />
+    </>
   );
 }
 
