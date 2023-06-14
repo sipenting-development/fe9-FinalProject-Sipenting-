fd4e8300be2ff4d13ceb3a32cbe6cf11553afbb2
dwiwahyup <dwiwahyu1235@gmail.com>
2023-06-14 23:43:45 +0700

    update redirect wa

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -20,7 +20,12 @@ const ChatBox = () => {
     <div>
       <Stack gap={3}>
         <div className="text-center mt-5">
-          <img src="https://res.cloudinary.com/damsz0xlx/image/upload/v1686235709/bro_eto8s3.png" alt="error" style={{ width: 350, height: 350 }} className="mb-3 mt-4" />
+          <img
+            src="https://res.cloudinary.com/damsz0xlx/image/upload/v1686235709/bro_eto8s3.png"
+            alt="error"
+            style={{ width: 350, height: 350 }}
+            className="mb-3 mt-4"
+          />
           <p className="fw-bold fs-4 mt-3">Silakan Melakukan Konsultasi</p>
           <Link to={"/"}>
             <Button variant="outline-success" className="btnmasuk rounded-5">
@@ -28,7 +33,13 @@ const ChatBox = () => {
             </Button>
           </Link>
           <Button variant="outline-success" className="btnmasuk rounded-5 mt5">
-            <a href="https://wa.me/+62895329165377">Lanjut Konsultasi</a>
+            <a
+              href="https://wa.me/+62895329165377"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Lanjut Konsultasi
+            </a>
           </Button>
         </div>
       </Stack>
