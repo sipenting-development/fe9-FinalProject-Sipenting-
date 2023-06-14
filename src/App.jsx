f6faa5c1e766f5e1c24a06fa3b9f0328dc81d09d
kharisma tara mukti <kharismataram@gmail.com>
2023-06-14 13:26:53 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
+import CekImt from "./pages/CekImt";
+import LandingPage from "./pages/LandingPage";
+import Konsultasi from "./pages/Konsultasi";
 import { Route, Routes } from "react-router-dom";
 import Artikel from "./pages/Artikel";
 import NotFound from "./pages/Notound";
+
+import ChatBox from "./pages/ChatBox";
+
 import LoginRegister from "./pages/LoginRegister";
-import "./App.css"
+import "./App.css";
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/" index element={<LandingPage />} />
         <Route path="/konsultasi" element={<Konsultasi />} />
         <Route path="/cekgizi" element={<CekImt />} />
+        <Route path="/chat" element={<ChatBox />} />
         <Route path="/artikel" element={<Artikel />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/login" element={<LoginRegister/>} />
-        <Route path="/register" element={<LoginRegister/>} />
+
+        <Route path="/login" element={<LoginRegister />} />
+        <Route path="/register" element={<LoginRegister />} />
       </Routes>
     </>
   );
-  }
+}
+
+export default App;
