88d79620f7f8fda01c0536266e4ac8b84675268e
kharisma tara mukti <kharismataram@gmail.com>
2023-06-14 19:58:25 +0700

    tambah rounded

--- src/pages/ChatBox.jsx
+++ src/pages/ChatBox.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Crisp } from "crisp-sdk-web";
-import { Stack } from "react-bootstrap";
+import { Stack, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./notfound.css";
 
 class ChatBox extends Component {
@@ -15,6 +16,11 @@ class ChatBox extends Component {
           <div className="text-center mt-5">
             <img src="https://res.cloudinary.com/damsz0xlx/image/upload/v1686235709/bro_eto8s3.png" alt="error" style={{ width: 350, height: 350 }} className="mb-3 mt-4" />
             <p className="fw-bold fs-4 mt-3">Silakan Melakukan Konsultasi</p>
+            <Link to={"/"}>
+              <Button variant="outline-success" className="btnmasuk rounded-5">
+                Kembali ke Beranda
+              </Button>
+            </Link>
           </div>
         </Stack>
       </div>
