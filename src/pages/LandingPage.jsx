8715e9587c0af2ed8c6ac833f1dbecdd37c66c21
WhataD?0wD <ditakusuma66@gmail.com>
2023-06-15 12:32:37 +0700

    styling data stunting

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -18,7 +18,7 @@ function LandingPage() {
             <h4 className="text-center sub-title">Data Stunting</h4>
             <h2 className="text-center fw-bold main-title">Balita di Indonesia</h2>
           </div>
-          <div className="d-flex justify-content-center gap-5">
+          <div className="d-flex flex-wrap justify-content-center gap-5">
             <DataSection img={"https://res.cloudinary.com/damsz0xlx/image/upload/v1683201270/Assets/data1_cl2jy3.png"} title={"24,4 %"} cardText1={"Stunting Balita"} cardText2={"(SSGI, 2021)"} />
             <DataSection img={"https://res.cloudinary.com/damsz0xlx/image/upload/v1683201270/Assets/data2_ficqic.png"} title={"7,1 %"} cardText1={"Wasting Balita"} cardText2={"(SSGI, 2021)"} />
             <DataSection img={"https://res.cloudinary.com/damsz0xlx/image/upload/v1683201270/Assets/data3_kjhtnn.png"} title={"17,7 %"} cardText1={"Underweight"} cardText2={"(SSGI, 2021)"} />
