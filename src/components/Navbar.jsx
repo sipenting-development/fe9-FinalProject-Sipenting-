0e6ddf069f531fc7e7a6cd78cec99a3c95074322
kharisma tara mukti <kharismataram@gmail.com>
2023-06-16 10:40:59 +0700

    link pada navbar dan footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,50 +15,22 @@ function NavbarSipenting() {
   return (
     <Navbar bg="white" expand="lg" fixed="top" className="nav shadow-sm">
       <Container>
-        <Navbar.Brand href="#logo">
-          <img
-            src="https://res.cloudinary.com/damsz0xlx/image/upload/v1683201272/Assets/navbar_bfjxem.png"
-            alt=""
-            width="150"
-          />
+        <Navbar.Brand as={Link} to="/">
+          <img src="https://res.cloudinary.com/damsz0xlx/image/upload/v1683201272/Assets/navbar_bfjxem.png" alt="" width="150" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="navbar-nav ms-auto justify-content-end flex-grow-1 pe-3">
-            <Nav.Link
-              as={NavLink}
-              exact={true.toString()}
-              to="/"
-              className="nav-link active pe-5"
-              style={getNavLinkStyle("/")}
-            >
+            <Nav.Link as={NavLink} exact={true.toString()} to="/" className="nav-link active pe-5" style={getNavLinkStyle("/")}>
               Beranda
             </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              exact={true.toString()}
-              to={isLogin ? "/konsultasi" : "/login"}
-              className="nav-link pe-5"
-              style={getNavLinkStyle("/konsultasi")}
-            >
+            <Nav.Link as={NavLink} exact={true.toString()} to={isLogin ? "/konsultasi" : "/login"} className="nav-link pe-5" style={getNavLinkStyle("/konsultasi")}>
               Konsultasi
             </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              exact={true.toString()}
-              to="/cekgizi"
-              className="nav-link pe-5"
-              style={getNavLinkStyle("/cekgizi")}
-            >
+            <Nav.Link as={NavLink} exact={true.toString()} to="/cekgizi" className="nav-link pe-5" style={getNavLinkStyle("/cekgizi")}>
               Cek Gizi
             </Nav.Link>
-            <Nav.Link
-              as={NavLink}
-              exact={true.toString()}
-              to="/artikel"
-              className="nav-link pe-5"
-              style={getNavLinkStyle("/artikel")}
-            >
+            <Nav.Link as={NavLink} exact={true.toString()} to="/artikel" className="nav-link pe-5" style={getNavLinkStyle("/artikel")}>
               Artikel
             </Nav.Link>
           </Nav>
